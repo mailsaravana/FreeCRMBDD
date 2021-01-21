cc4131a1cd70c1f37c58cc90f8196d168beac696
mailsaravana@gmail.com <mailsaravana@gmail.com>
2021-01-21 20:38:51 +0000

    runner

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -2,14 +2,16 @@ package Runner;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
-import io.cucumber.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
+
 
 
 
 	@RunWith(Cucumber.class) 
 	@CucumberOptions(
-			features="/Users/Admin/eclipse-workspace/FreeCRMBDDFramework/src/main/java/Features/login.feature",
-			glue={"StepDefinitions"}
+			features="/Users/Admin/eclipse-workspace/FreeCRMBDDFramework/src/main/java/Features/login.feature"
+			,glue={"stepDefinitions"}
+		//	,format = {"pretty","html:test-output"}
 			
 			) 
 
