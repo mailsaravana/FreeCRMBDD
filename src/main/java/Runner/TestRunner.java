83c0d7f4fcce4ede4e5ab3e3ac05e276ce88b674
mailsaravana@gmail.com <mailsaravana@gmail.com>
2021-01-21 20:14:38 +0000

    Test runner commit

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -1,17 +1,15 @@
 package Runner;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
- 
-@RunWith(Cucumber.class)
-@CucumberOptions(
- features = "Feature"
- ,glue={"stepDefinition"}
- )
+import io.cucumber.junit.Cucumber;
+
+	@RunWith(Cucumber.class) 
+	@CucumberOptions(features="/FreeCRMBDDFramework/src/main/java/Features/login.feature",
+	glue={"StepDefinition"}) 
 
 
+		
 public class TestRunner {
 
 	
